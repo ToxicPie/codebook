6c1a7422cdc76d10238057182ad1da7731044978
toxicpie <kent05011607@gmail.com>
2020-06-28 01:43:35 +0800

    improved test unit for tonelli-shanks algorithm

--- tests/number-theory/tonelli-shanks.cpp
+++ tests/number-theory/tonelli-shanks.cpp
@@ -2,7 +2,7 @@
 
 #include <utility>
 #include <sstream>
-#include <vector> 
+#include <vector>
 
 
 using namespace std;
@@ -16,22 +16,21 @@ namespace _sqrt {
 
 namespace math {
     void sqrt_test(int count) {
-        // randint rand(0, INT32_MAX);
         randint rand(0, 1LL << 60);
         const vector<ll> MODS = {2, 7, 21673, 26497, 22621, 21817, 28393, 26821, 30181, 65537, 977680993, 971939533, 970479637, 998244353, 1000000007, 1041012121, 1000000000039, 2748779069441, 1000000000000037, 1945555039024054273};
         for(int i = 1; i <= count; i++) {
-            _sqrt::Mod a;
-            a.MOD = MODS[i % MODS.size()];
-            do {
-                a = rand();
-            } while(_sqrt::legendre(a) == -1);
-            _sqrt::Mod x =  _sqrt::sqrt(a);
-            if(!(x * x == a)) {
+            _sqrt::Mod ans, test;
+            ans.MOD = MODS[i % MODS.size()];
+            ans = rand();
+            test = ans * ans;
+            _sqrt::Mod ret =  _sqrt::sqrt(test);
+            if(ret * ret != test) {
                 stringstream ss;
                 ss << "Test case " << i << ": ";
-                ss << "a = " << (ll)a << ", ";
-                ss << "x = " << (ll)x << ", ";
-                ss << "p = " << a.MOD << "";
+                ss << "input a = " << (ll)test << ", ";
+                ss << "input p = " << test.MOD << ", ";
+                ss << "expected " << (ll)ans << " or " << (ll)-ans << ", ";
+                ss << "found " << (ll)ret << "";
                 throw wrong_answer_error(ss.str());
             }
         }
