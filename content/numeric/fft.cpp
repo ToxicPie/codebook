734cf626776d77af40e5873a1be8be390ee22c8b
toxicpie <kent05011607@gmail.com>
2020-08-16 02:15:46 +0800

    removed some extra lines

--- content/numeric/fft.cpp
+++ content/numeric/fft.cpp
@@ -4,21 +4,18 @@ void work(int n, vector<T>& a, vector<T>& rt, bool inv) {
 		for (int bit = n; !(r & bit); bit >>= 1, r ^= bit);
 		if (r > i) swap(a[i], a[r]);
 	}
-	for (int len = 2; len <= n; len <<= 1) {
-		for (int i = 0; i < n; i += len) {
+	for (int len = 2; len <= n; len <<= 1)
+		for (int i = 0; i < n; i += len)
 			for (int j = 0; j < len / 2; j++) {
 				int pos = n / len * (inv ? len - j : j);
 				T u = a[i + j], v = a[i + j + len / 2] * rt[pos];
 				a[i + j] = u + v, a[i + j + len / 2] = u - v;
 			}
-		}
-	}
 	if (inv) {
 		T minv = T(1) / T(n);
 		for (T& x : a) x *= minv;
 	}
 }
-
 void FFT(vector<complex<double>>& a, bool inv) {
 	int n = a.size();
 	vector<complex<double>> rt(n + 1);
@@ -27,7 +24,6 @@ void FFT(vector<complex<double>>& a, bool inv) {
 		rt[i] = { cos(arg * i), sin(arg * i) };
 	work(n, a, rt, inv);
 }
-
 void NTT(vector<Mod>& a, bool inv, Mod p_root) {
 	int n = a.size();
 	Mod root = p_root ^ (MOD - 1) / n;
