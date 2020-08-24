db16b113f7339634a118dbe6185d28e8d49df06e
toxicpie <kent05011607@gmail.com>
2020-08-25 03:55:07 +0800

    fixed __builtin_ctz returning unexpected result with long long

--- content/math/number-theory/miller-rabin.cpp
+++ content/math/number-theory/miller-rabin.cpp
@@ -3,7 +3,7 @@ bool is_prime() {
 	if (MOD <= 1 || MOD % 2 == 0) return MOD == 2;
 	// Mod A[] = {2, 7, 61};
 	Mod A[] = {2,325,9375,28178,450775,9780504,1795265022};
-	int s = __builtin_ctz(MOD - 1), i;
+	int s = __builtin_ctzll(MOD - 1), i;
 	for (Mod a : A) {
 		Mod x = a ^ (MOD >> s);
 		for (i = 0; i < s && ll(x + 1) > 2; i++, x *= x);
